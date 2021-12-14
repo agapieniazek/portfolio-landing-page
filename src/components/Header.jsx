ca71a35cfc0938e93174137e50ca3fd7abcdf594
Agnieszka <dzieszkol@gmail.com>
2021-12-14 20:22:19 +0100

    zmiana obrazkow

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import Navigation from "./Navigation";
+import img1 from "../images/header.jpg";
+import img2 from "../images/hello.png";
 
 const Header = () => {
   return ( 
     <>
-    <div className="header__navigation">
     {<Navigation/>}
-    </div>
-    <div className="header__img">
-      <h3>Hi!</h3>
+    <div className="header__main main">
+      <div className="header__info main__info">
+      <img src={img2} alt="" />
       <h1>I'm Aga Pieniążek</h1>
       <h2>nice to meet you</h2>
+      </div>
+      <div className="header__img main__img">
+      <img src={img1} alt="" />
+      </div>
     </div>
     </>
    );
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import "../styles/_navigation.scss";
+import planet from "../images/planet.png";
+
+const navList = [
+  {name: "start", desc:"Home", path:"/", exact: true},
+  {name: "about", desc:"About me", path:"/about"},
+  {name: "toolkit", desc:"My toolkit", path:"/toolkit"},
+  {name: "contact", desc:"Contact", path:"/contact"},
+  {name: "game", desc:"Let's play", path:"/game"}
+]
 
 const Navigation = () => {
+  const menu = navList.map(item => {
+    return (
+      <li key={item.name} className="listElem">
+        <NavLink to={item.path} exact={item.exact ? item.exact : false}>{item.desc}</NavLink>
+      </li>
+    )
+  })
   return ( 
-    <>
-    Hallo
-    </>
+    <nav>
+    <img src={planet} alt="logo"/>
+      <ul>
+       {menu}
+      </ul>
+    </nav>
    );
 }
  
